badd4121215d99fb2fdad124192e2cfaf6396782
Shreya Basu <michishreya@gmail.com>
2018-07-10 17:37:18 +0000

    Done.

--- macarena.rb
+++ macarena.rb
@@ -5,26 +5,27 @@ puts ""
 # Every line should have one instruction using puts.
 # The first instruction is correct. 
 
-# Step 1: GOOD
+# Step 1: 
 puts "Step 1: Right arm out in front of you, palm facing down."
 
-# Step 2: FIXME
-puts "Step 2: Turn your right palm up."
+# Step 2: 
+puts "Step 2: Left arm out in front of you, palm facing down."
 
-# Step 3: FIXME
-puts "Step 3: Place your right hand on your left shoulder."
+#Step 3:
+puts "Step 3: Turn your right palm up."
 
-# Step 4: FIXME
+# Step 4: 
 puts "Step 4: Turn your left palm up."
 
-# Step 5: FIXME
-puts "Step 5: Bring your right hand up and around to the back of your head."
+# Step 5: 
+puts "Step 5: Place your right hand on your left shoulder."
 
-# Step 6: FIXME
-puts "Step 6: Left arm out in front of you, palm facing down."
 
-# Step 7: FIXME
-puts "Step 7: Place your left hand on your left shoulder."
+# Step 6: 
+puts "Step 6: Place your left hand on your right shoulder."
 
-# Step 8: FIXME
-puts "Step 8: The Last Step"
+# Step 7: 
+puts "Step 7: Bring your right hand up and around to the back of your head."
+
+# Step 8:
+puts "Step 8: Bring your left hand up and around to the back your head "
